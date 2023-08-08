a814ca9a9af6e7cdd8b24e74c9966305155a8bcd
Anshul Yadav <anshulyadav98@gmail.com>
2023-08-08 21:49:23 +0530

    Added a demo

--- 2. MBA-Backend-Session-2/Routers/Theatre.route.js
+++ 2. MBA-Backend-Session-2/Routers/Theatre.route.js
@@ -1,16 +1,33 @@
-
-
-
-const theatreController = require("../Controllers/Theatre.controlelr");
+const theatreController = require('../Controllers/Theatre.controlelr')
+const authJwt = require('../Middlewares/authJwt')
 
 module.exports = function (app) {
-    app.get("/MovieBooking/api/v1/theatres", theatreController.getAllTheatres);
-    // app.get("/MovieBooking/api/v1/theatres/:theatreId", theatreController.getTheatre);
-    app.get("/MovieBooking/api/v1/alltheatres" , theatreController.AllTheatres)
-    app.get("/MovieBooking/api/v1/MoviesInTheatre/:theatreName/:movieId" , theatreController.getMoviesInTheatre)
-    app.post("/MovieBooking/api/v1/theatres", theatreController.addTheatre);
-    app.put("/MovieBooking/api/v1/theatres/:theatreId", theatreController.updateTheatre);
-    app.delete("/MovieBooking/api/v1/theatres/:theatreId", theatreController.deleteTheatre);
-    app.delete("/MovieBooking/api/v1/theatres/deleteMovie/:theatreId/:movieId", theatreController.removeMoviesFromTheatre);
-    app.put("/MovieBooking/api/v1/addMovieInTheatre/:theatreId" , theatreController.addMoviesInTheatreTwo)
+  app.get(
+    '/MovieBooking/api/v1/theatres',
+    [authJwt.verifyToken],
+    theatreController.getAllTheatres
+  )
+  // app.get("/MovieBooking/api/v1/theatres/:theatreId", theatreController.getTheatre);
+  app.get('/MovieBooking/api/v1/alltheatres', theatreController.AllTheatres)
+  app.get(
+    '/MovieBooking/api/v1/MoviesInTheatre/:theatreName/:movieId',
+    theatreController.getMoviesInTheatre
+  )
+  app.post('/MovieBooking/api/v1/theatres', theatreController.addTheatre)
+  app.put(
+    '/MovieBooking/api/v1/theatres/:theatreId',
+    theatreController.updateTheatre
+  )
+  app.delete(
+    '/MovieBooking/api/v1/theatres/:theatreId',
+    theatreController.deleteTheatre
+  )
+  app.delete(
+    '/MovieBooking/api/v1/theatres/deleteMovie/:theatreId/:movieId',
+    theatreController.removeMoviesFromTheatre
+  )
+  app.put(
+    '/MovieBooking/api/v1/addMovieInTheatre/:theatreId',
+    theatreController.addMoviesInTheatreTwo
+  )
 }
--- 2. MBA-Backend-Session-2/server.js
+++ 2. MBA-Backend-Session-2/server.js
@@ -1,33 +1,49 @@
-
-
-const express = require('express');
+const express = require('express')
 const bodyParser = require('body-parser')
 const serverConfig = require('./Configs/server.config')
 const dbConfig = require('./Configs/db.config')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const Movies = require('./Models/Movies')
+const Users = require('./Models/Users')
 
-
-const expressApp = express();
+const expressApp = express()
 
 expressApp.use(bodyParser.json())
-expressApp.use(bodyParser.urlencoded({extended: true}))
+expressApp.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect(dbConfig.DB_URL)
 const db = mongoose.connection
-db.on("error", () => {
-    console.log("Error while connecting to DB")
+db.on('error', () => {
+  console.log('Error while connecting to DB')
 })
-db.once("open", () => {
-    console.log("Successfully Connected to mongo DB")
+db.once('open', () => {
+  console.log('Successfully Connected to mongo DB')
+  init()
 })
 
+/**
+ * This function will initialize the state of the move booking database
+ */
+async function init() {
+  await Users.collection.drop()
+  const user = await Users.create({
+    name: 'Anshul',
+    userId: 'admin',
+    email: 'anshul@gmail.com',
+    userType: 'ADMIN',
+    password: bcrypt.hashSync('Welcome', 8),
+  })
 
-require('./Routers/Movie.route')(expressApp);
-require("./Routers/Theatre.route")(expressApp);
-require("./Routers/Auth.route")(expressApp);
-require("./Routers/User.route")(expressApp);
-require("./Routers/Booking.route")(expressApp);
+  console.log('Admin user created successfully')
+}
+
+require('./Routers/Movie.route')(expressApp)
+require('./Routers/Theatre.route')(expressApp)
+require('./Routers/Auth.route')(expressApp)
+require('./Routers/User.route')(expressApp)
+require('./Routers/Booking.route')(expressApp)
 
 expressApp.listen(serverConfig.PORT, () => {
-    console.log(`Application started on port ${serverConfig.PORT}`)
+  console.log(`Application started on port ${serverConfig.PORT}`)
 })
