4586636881580ab1d4734c24faf42c2b54f65002
Anshul Yadav <anshulyadav98@gmail.com>
2023-08-12 12:26:01 +0530

    Integrated the Notification Service

---
+++ 5. Notification-Service/notifier/emailService.js
@@ -0,0 +1,10 @@
+const nodemailer = require('nodemailer')
+
+module.exports = nodemailer.createTransport({
+  service: 'gmail',
+  debug: true,
+  auth: {
+    user: 'anshulyadav98@gmail.com',
+    pass: '<Enter-your-app-password>',
+  },
+})
